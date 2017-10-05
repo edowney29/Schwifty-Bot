1f4cf7e2768d7919240823de7aea1a5e8fb8a0db
Eric Downey <edowney29@gmail.com>
2017-10-05 17:09:05 -0400

    Fix again

--- app/discordbot.js
+++ app/discordbot.js
@@ -59,8 +59,8 @@ client.on('message', message => {
 	if (_.includes(msg, 'tz')) {
 		msg = msg.toUpperCase();
 		var split = msg.split(' ');
-		var split2 = split[1].split(':')
-		var naiveDate = new tc.DateTime(2001, 09, 11, split2[0], split2[1], 00);		
+		var time = split[1].split(':')
+		var naiveDate = new timezone.DateTime(2001, 09, 11, time[0], time[1], 00);		
 		var str = new timezone.DateTime(naiveDate)
 		if (str) {
 			message.reply(str.toIsoString())
