72106b43fa5d6b7a1827c42a50981ddd4de3941a
Eric Downey <edowney29@gmail.com>
2017-10-05 22:49:48 -0400

    asdf

--- app/discordbot.js
+++ app/discordbot.js
@@ -4,8 +4,8 @@ const google = require('googleapis')
 const _ = require('lodash')
 
 const client = new discord.Client()
-const GOOGLE_KEY = process.env.GOOGLE_KEY
-const DISCORD_KEY = process.env.DISCORD_KEY
+const GOOGLE_KEY = 'MzMwNTM5ODQ0ODg5NDc3MTIx.DDijpw.McCPms6FKHgvjLW9fCl8NQacWfg'//process.env.GOOGLE_KEY
+const DISCORD_KEY = 'AIzaSyCu_jTK-L3GLefF337NJmmK4ksoI8YJxnc'//process.env.DISCORD_KEY
 
 const engine = random.engines.mt19937().autoSeed()
 
@@ -55,9 +55,50 @@ client.on('message', message => {
 		message.reply(d20)
 	}
 
+	if (_.includes(msg, 'tz')) {
+		_.toUpper(msg)
+		var split = msg.split(' ')
+		var index = split.indexOf('TZ')
+		var time = split[index + 1].split(":")
+		var hour = parseInt(time[0])
+
+		var offset = getZone(split[index + 2])
+		hour = (hour + (offset * -1)) % 12
+
+		var est = (hour + getZone('EST')) % 12
+		var cst = (hour + getZone('CST')) % 12
+		var mdt = (hour + getZone('MST')) % 12
+		var pdt = (hour + getZone('PST')) % 12
+		var ist = (hour + getZone('IST')) % 12
+
+		message.reply(
+			'EST: ' + est + ':' + time[1] +
+			'CST: ' + cst + ':' + time[1] +
+			'MST: ' + mst + ':' + time[1] +
+			'PST: ' + pst + ':' + time[1] +
+			'IST: ' + ist + ':' + time[1]
+		)
+	}
+
+
 })
 
 client.login(DISCORD_KEY)
+
+function getZone(zone) {
+	if (zone == 'EST')
+		return -5
+	if (zone == 'CST')
+		return -6
+	if (zone == 'MST')
+		return -7
+	if (zone == 'PST')
+		return -8
+	if (zone == 'IST')
+		return 1
+	if (zone == 'GMT')
+		return 0
+}
 
 function getData() {
 	return new Promise((resolve, reject) => {
