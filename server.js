cabd3872edf1e2f8dc455d45358c466d9bb8c613
edowney29 <edowney29@gmail.com>
2017-11-14 22:00:50 -0500

    asdfasdf

--- server.js
+++ server.js
@@ -361,3 +361,19 @@ function atan2_approximation2(x, y) {
   }
   return atan
 }
+
+function sqrt_approximation(n, g) {
+  if (!g) {
+      // Take an initial guess at the square root
+      g = n / 2.0;
+  }
+  var d = n / g;              // Divide our guess into the number
+  var ng = (d + g) / 2.0;     // Use average of g and d as our new guess
+  if (g == ng) {          
+      // The new guess is the same as the old guess; further guesses
+      // can get no more accurate so we return this guess
+      return g;
+  }
+  // Recursively solve for closer and closer approximations of the square root
+  return squirt(n, ng);
+}
