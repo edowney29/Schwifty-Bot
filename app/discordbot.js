c9c2adec097e569b3de74d2d591ed14a23a39d5e
edowney29 <edowney29@gmail.com>
2017-11-19 15:25:55 -0500

    asdfsd

--- app/discordbot.js
+++ app/discordbot.js
@@ -3,7 +3,6 @@ const random = require('random-js')
 const googleapi = require('googleapis')
 const moment = require('moment-timezone');
 const _ = require('lodash')
-//const sqlite = require('sqlite')
 
 const client = new discord.Client()
 const DISCORD_KEY = process.env.DISCORD_KEY
