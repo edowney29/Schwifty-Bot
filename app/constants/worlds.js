e96bcb90ca6daa205d7d4f461ec9d53e82401dc2
edowney29 <edowney29@gmail.com>
2017-12-07 01:37:26 -0500

    Worlds

---
+++ app/constants/worlds.js
@@ -0,0 +1,20 @@
+module.exports.worlds = worlds;
+
+var worlds = [{
+
+    worldid: 'test',
+    zones: [{
+        zoneid: 'safe',
+        players: []
+    }, {
+        zoneid: 'south',
+        players: []
+    }],
+
+    worldid: 'stater',
+    zones: {
+        zoneid: 'safe',
+        players: []
+    }
+
+}]
