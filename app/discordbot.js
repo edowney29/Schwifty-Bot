6cb56c42215f84a503475663bf675db92bafaac8
Eric Downey <edowney29@gmail.com>
2018-01-07 19:19:24 -0500

    fixes

--- app/discordbot.js
+++ app/discordbot.js
@@ -48,25 +48,26 @@ client.on('message', message => {
 			ytdl.getInfo(url, (err, info) => {
 				if (err) throw err
 				var audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-				var file = audioFormats[0].url
-				var stream = fs.createReadStream(
-					request
-						.get(file)
-						.on('error', err => {
+				var fileurl = audioFormats[0].url
+				request
+					.get(fileurl)
+					.on('error', err => {
+						console.log(err)
+						message.reply('Error getting file!')
+					})
+					.on('end', () => {
+						var stream = fs.createReadStream('./public/' + message.member.guild + '.' + audioFormats[0].container)
+						stream.on('error', () => {
 							console.log(err)
-							message.reply('Error getting file!')
+							message.reply('Error streaming!')
 						})
-						.pipe(fs.creatWriteStream('./song.' + audioFormats[0].container))
-				)
-				stream.on('end', () => {
-					var connection = message.member.voiceChannel.connection
-					var streamOptions = { seek: 0, volume: 1, passes: 1, bitrate: 48000 }
-					var sd = connection.playStream(stream, streamOptions)
-				})
-				stream.on('error', () => {
-					console.log(err)
-					message.reply('Error streaming!')
-				})
+						stream.on('end', () => {
+							var connection = message.member.voiceChannel.connection
+							var streamOptions = { seek: 0, volume: 1, passes: 1, bitrate: 48000 }
+							var sd = connection.playStream(stream, streamOptions)
+						})
+					})
+					.pipe(fs.createWriteStream('./public' + message.member.guild + '.' + audioFormats[0].container))
 			})
 
 			message.reply('Playing: ' + queueNames[0])
