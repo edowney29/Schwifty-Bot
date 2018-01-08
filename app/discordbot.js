889bed56faf29612c15588614fb9ee80f7f5bade
Eric Downey <edowney29@gmail.com>
2018-01-07 20:04:44 -0500

    asdfsadf

--- app/discordbot.js
+++ app/discordbot.js
@@ -57,9 +57,15 @@ client.on('message', message => {
 					})
 					.on('end', () => {
 						var stream = fs.createReadStream('./public/' + message.member.guild + '.' + audioFormats[0].container)
-						var connection = message.member.voiceChannel.connection
-						var streamOptions = { seek: 0, volume: 1, passes: 1, bitrate: 48000 }
-						var sd = connection.playStream(stream, streamOptions)
+						stream.on('error', () => {
+							console.log(err)
+							message.reply('Error streaming!')
+						})
+						stream.on('readable', () => {
+							var connection = message.member.voiceChannel.connection
+							var streamOptions = { seek: 0, volume: 1, passes: 1, bitrate: 48000 }
+							var sd = connection.playStream(stream, streamOptions)
+						})
 					})
 					.pipe(fs.createWriteStream('./public/' + message.member.guild + '.' + audioFormats[0].container))
 			})
