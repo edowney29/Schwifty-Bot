14a32af38edac7103903fc7f7555a26313010d66
Eric Downey <edowney29@gmail.com>
2018-01-09 18:58:54 -0500

    fixes

--- app/mmo/socketio.js
+++ app/mmo/socketio.js
@@ -64,14 +64,142 @@ function startServer() {
 		socket.on('disconnect', disconnect)
 		socket.on('error', (error) => { console.log(`[RECV - Server error] : ${playerToken} : ${error}`) })
 
+		function playerReg(username, email, pass) {
+			var newUser = {
+				username,
+				email,
+				pass,
+				reg: false,
+			}
+			var newMovements = {
+				username,
+				positionx: 1297.0,
+				positiony: -1125,
+			}
+
+			database.collection('users').findOne({
+				$or: [{
+					username: username
+				}, {
+					email: email
+				}]
+			}, (err, doc) => {
+				if (err) {
+					socket.emit('player-menu', 'err')
+				} else if (doc) {
+					socket.emit('player-menu', 'dub')
+				} else {
+					users.insertOne(newUser, (err, res) => {
+						database.collection('movements').insertOne(newMovements, (err, res) => {
+							if (err) {
+								socket.emit('player-menu', 'err')
+							} else {
+								socket.emit('player-menu', 'good')
+							}
+						})
+					})
+				}
+			})
+		}
+
+		function playerLogin(username) {
+			var pass = true
+			_.forEach(clients, client => {
+				if (username == client.username) {
+					pass = false
+					socket.emit('player-menu', 'log')
+				}
+			})
+
+			if (pass) {
+				var users = database.collection('users')
+				users.findOne({
+					username: username
+				}, (err, doc) => {
+					if (err) {
+						socket.emit('player-menu', 'err')
+					} else if (doc) {
+						socket.emit('player-menu', doc.pass)
+						playerToken = username
+					} else {
+						socket.emit('player-menu', 'not')
+					}
+				})
+			}
+		}
+
+		function startUp(username) {
+			if (username) {
+				db.collection('movements').findOne({
+					username: username,
+				}, (err, doc) => {
+					if (err) {
+
+					} else {
+						socket.join('start');
+						var client = {
+							username: doc.username,
+							positionx: doc.positionx,
+							positiony: doc.positiony,
+							positionz: doc.positionz,
+							rotationx: doc.rotationx,
+							rotationy: doc.rotationy,
+							rotationz: doc.rotationz,
+							rotationw: doc.rotationw,
+							health: 0,
+							socket: socket,
+							room: 'start'
+						}
+						// SETUP YOUR PLAYER
+						socket.emit('start-up',
+							client.username,
+							client.positionx,
+							client.positiony,
+							client.positionz,
+							client.rotationx,
+							client.rotationy,
+							client.rotationz,
+							client.rotationw,
+							client.health
+						)
+						clients.push(client)
+					}
+				})
+			}
+		}
+
+		function playerMove(username, positionx, positiony, playerMoving, moveH, moveV, lastmovex, lastmovey, world, zone) {
+			var client = _.find(clients, { username: username })
+
+			if (client) {
+				client.username = username
+				client.positionx = positionx
+				client.positiony = positiony
+				//client.world = world
+				//client.zone = zone
+
+				io.in(client.room).emit('player-move',
+					username,
+					positionx,
+					positiony,
+					playerMoving,
+					moveH,
+					moveV,
+					lastmovex,
+					lastmovey
+				)
+				socket.emit('ack-move')
+			}
+		}
+
 		function message(username, message) {
-			//console.log('[RECV - Message] ' + username + ': ' + message)
-			socket.broadcast.emit('player-message', username, message)
-			socket.emit('player-message', username, message);
+			var client = _.find(clients, { username: username })
+			if (client) {
+				io.in(client.room).emit('player-message', username, message)
+			}
 		}
 
 		function playerAttack(username, attacking) {
-			//console.log('[RECV - Attack] ' + username + ': ' + attacking)
 			var client = _.find(clients, { username: username })
 			if (client) {
 				io.in(client.room).emit('player-attack', username, attacking)
