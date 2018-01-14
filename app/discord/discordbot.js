0e2520d3727576d190daf1e106a224bad19fcd5f
Eric Downey <edowney29@gmail.com>
2018-01-14 12:39:07 -0500

    final

--- app/discord/discordbot.js
+++ app/discord/discordbot.js
@@ -8,25 +8,41 @@ const googleapis = require('googleapis')
 const request = require('request')
 const fs = require('fs')
 
-const client = new discord.Client()
 const DISCORD_KEY = process.env.DISCORD_KEY
 const GOOGLE_KEY = process.env.GOOGLE_KEY
 
+const client = new discord.Client()
 const engine = random.engines.mt19937().autoSeed()
-
-var queueIds = [], queueNames = []
-
-client.on('ready', () => {
-
+const youtube = googleapis.youtube({
+	version: 'v3',
+	auth: GOOGLE_KEY
 })
 
+var servers = []
+
 client.on('message', message => {
-	console.log(message.content)
+	var index = _.findIndex(servers, { id: message.guild.id })
+	if (index == -1) {
+		var server = {
+			id: message.guild.id,
+			queue: {
+				ids: [],
+				names: []
+			}
+		}
+		servers.push(server)
+		index = _.findIndex(servers, { id: message.guild.id })
+	}
+
 	var string = _.toLower(message.content)
+	console.log(`[${message.guild.id}] ${message.member.id}: ${message.content}`)
 
 	if (_.includes(string, '!join')) {
 		message.delete()
 		if (message.member.voiceChannel) {
+			if (message.member.voiceChannel.connection) {
+				message.member.voiceChannel.connection.disconnect()
+			}
 			message.member.voiceChannel.join()
 		} else {
 			message.reply('You must be in a voice channel.')
@@ -50,17 +66,19 @@ client.on('message', message => {
 								name = item.snippet.title
 							}
 						})
-						queueIds.push(id)
-						queueNames.push(name)
+						servers[index].queue.ids.push(id)
+						servers[index].queue.names.push(name)
 						message.reply(`Song queued: http://www.youtube.com/watch?v=${id}`)
 
-						if (!message.member.voiceChannel.connection.dispatcher) {
-							var url = `http://www.youtube.com/watch?v=${queueIds[0]}`
-							var dispatcher = message.member.voiceChannel.connection.dispatcher
-							if (dispatcher) {
-								dispatcher.end()
-							}
-							playSong(message, url)
+						var dispatcher = message.guild.voiceConnection.dispatcher
+						if (!dispatcher) {
+							var url = `http://www.youtube.com/watch?v=${id}`
+							playSong(message, url, index)
+								.then(() => {
+									message.reply('Playing: ' + servers[index].queue.names[0])
+									servers[index].queue.ids = _.drop(servers[index].queue.ids, 1)
+									servers[index].queue.names = _.drop(servers[index].queue.names, 1)
+								})
 						}
 					})
 			})
@@ -68,13 +86,18 @@ client.on('message', message => {
 
 	if (_.includes(string, '!next')) {
 		message.delete()
-		if (queueIds.length > 0) {
-			var url = `http://www.youtube.com/watch?v=${queueIds[0]}`
-			var dispatcher = message.member.voiceChannel.connection.dispatcher
+		if (servers[index].queue.ids.length > 0) {
+			var url = `http://www.youtube.com/watch?v=${servers[index].queue.ids[0]}`
+			var dispatcher = message.guild.voiceConnection.dispatcher
 			if (dispatcher) {
 				dispatcher.end()
 			}
-			playSong(message, url)
+			playSong(message, url, server)
+				.then(() => {
+					message.reply('Playing: ' + servers[index].queue.names[0])
+					servers[index].queue.ids = _.drop(servers[index].queue.ids, 1)
+					servers[index].queue.names = _.drop(servers[index].queue.names, 1)
+				})
 		}
 		else {
 			message.reply('No songs queued.')
@@ -83,7 +106,7 @@ client.on('message', message => {
 
 	if (_.includes(string, '!resume')) {
 		message.delete()
-		var dispatcher = message.member.voiceChannel.connection.dispatcher
+		var dispatcher = message.guild.voiceConnection.dispatcher
 		if (dispatcher.paused) {
 			dispatcher.resume()
 		}
@@ -91,7 +114,7 @@ client.on('message', message => {
 
 	if (_.includes(string, '!pause')) {
 		message.delete()
-		var dispatcher = message.member.voiceChannel.connection.dispatcher
+		var dispatcher = message.guild.voiceConnection.dispatcher
 		if (!dispatcher.paused) {
 			dispatcher.pause()
 		}
@@ -99,16 +122,17 @@ client.on('message', message => {
 
 	if (_.includes(string, '!check')) {
 		message.delete()
-		var str = `${queueNames.length} songs queued \n${_.join(queueNames, '\n')}`
+		var str = `${servers[index].queue.names.length} songs queued \n${_.join(servers[index].queue.names, '\n')}`
 		message.reply(str)
 	}
 
 	if (_.includes(string, '!remove')) {
 		message.delete()
-		if (queueIds.length > 0) {
-			message.reply(`Song removed:  + ${queueNames[queueNames.length - 1]}`)
-			queueIds = _.drop(queueIds, 1)
-			queueNames = _.drop(queueNames, 1)
+		var num = servers[index].queue.ids.length
+		if (num > 0) {
+			message.reply(`Song removed:  + ${servers[index].queue.names[num - 1]}`)
+			_.drop(servers[index].queue.ids, 1)
+			_.drop(servers[index].queue.names, 1)
 		}
 	}
 
@@ -216,12 +240,7 @@ function getZone(zone) {
 }
 
 function searchVideos(videoTitle) {
-	new Promise((resolve, reject) => {
-		var youtube = googleapis.youtube({
-			version: 'v3',
-			auth: GOOGLE_KEY
-		})
-
+	return new Promise((resolve, reject) => {
 		youtube.search.list({
 			part: 'snippet',
 			q: videoTitle,
@@ -239,7 +258,7 @@ function searchVideos(videoTitle) {
 }
 
 function listVideos(videos) {
-	new Promise((resolve, reject) => {
+	return new Promise((resolve, reject) => {
 		var ids = []
 		_.forEach(videos.items, item => {
 			ids.push(item.id.videoId)
@@ -260,35 +279,37 @@ function listVideos(videos) {
 	})
 }
 
-function playSong(message, url) {
-	ytdl.getInfo(url, (err, info) => {
-		if (err) {
-			console.log(err)
-			return
-		}
-		else {
-			var audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-			var fileurl = audioFormats[0].url
-			request
-				.get(fileurl)
-				.on('error', err => {
-					console.log(err)
-					return
-				})
-				.on('end', () => {
-					var streamOptions = { seek: 0, volume: 0.2, passes: 1, bitrate: 64 * 1024 }
-					var connection = message.member.voiceChannel.connection
-					var sd = connection.playFile(`./public/${message.guild}.${audioFormats[0].container}`, streamOptions)
-					sd.on('error', err => {
+function playSong(message, url, index) {
+	return new Promise((resolve, reject) => {
+		ytdl.getInfo(url, (e, info) => {
+			if (e) {
+				console.log(e)
+				reject(e)
+			}
+			else {
+				var audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+				var fileurl = audioFormats[0].url
+				request
+					.get(fileurl)
+					.on('error', err => {
 						console.log(err)
 					})
-					sd.on('start', () => {
-						message.reply('Playing: ' + queueNames[0])
-						queueIds = _.drop(queueIds, 1)
-						queueNames = _.drop(queueNames, 1)
+					.on('end', () => {
+						var streamOptions = { seek: 0, volume: 0.2, passes: 1, bitrate: 64 * 1024 }
+						var connection = message.member.voiceChannel.connection
+						var sd = connection.playFile(`./public/${message.guild}.${audioFormats[0].container}`, streamOptions)
+						sd.on('error', error => {
+							reject(error)
+						})
+						sd.on('end', () => {
+							console.log('END')
+						})
+						sd.on('start', () => {
+							resolve()
+						})
 					})
-				})
-				.pipe(fs.createWriteStream(`./public/${message.guild}.${audioFormats[0].container}`))
-		}
+					.pipe(fs.createWriteStream(`./public/${message.guild}.${audioFormats[0].container}`))
+			}
+		})
 	})
 }
