34df561d81697edcb785b539ba1398e8698da523
Eric Downey <edowney29@gmail.com>
2019-05-12 19:19:48 -0400

    Update discord.js

--- app/discord.js
+++ app/discord.js
@@ -1,23 +1,23 @@
-const discord = require('discord.js');
-const moment_tz = require('moment-timezone');
-const moment = require('moment');
-const _ = require('lodash');
+const discord = require("discord.js");
+const moment_tz = require("moment-timezone");
+const moment = require("moment");
+const _ = require("lodash");
 
-const helper = require('./discordHelper');
+const helper = require("./discordHelper");
 
 const { DISCORD_KEY } = process.env;
 const client = new discord.Client();
 
 const servers = [];
 
-client.on('ready', () => {
-  client.user.setActivity('the game of life');
+client.on("ready", () => {
+  client.user.setActivity("the game of life");
 });
 
 // eslint-disable-next-line complexity
-client.on('message', (message) => {
+client.on("message", message => {
   let index = _.findIndex(servers, {
-    id: message.guild.id,
+    id: message.guild.id
   });
 
   if (index === -1) {
@@ -25,8 +25,8 @@ client.on('message', (message) => {
       id: message.guild.id,
       queue: {
         ids: [],
-        names: [],
-      },
+        names: []
+      }
     };
     servers.push(server);
     index = servers.length - 1;
@@ -35,7 +35,7 @@ client.on('message', (message) => {
   const string = _.toLower(message.content);
   console.log(`[${message.guild.id}] ${message.member.id}: ${message.content}`);
 
-  if (_.includes(string, '!join')) {
+  if (_.includes(string, "!join")) {
     if (message.member.voiceChannel) {
       if (message.member.voiceChannel.connection) {
         message.member.voiceChannel.connection.disconnect();
@@ -43,18 +43,18 @@ client.on('message', (message) => {
 
       message.member.voiceChannel.join();
     } else {
-      message.reply('You must be in a voice channel.');
+      message.reply("You must be in a voice channel.");
     }
   }
 
-  if (_.includes(string, '!play')) {
+  if (_.includes(string, "!play")) {
     if (message.member.voiceChannel) {
-      let videoTitle = _.split(string, ' ');
+      let videoTitle = _.split(string, " ");
       videoTitle = _.drop(videoTitle, 1);
-      videoTitle = _.join(videoTitle, ' ');
+      videoTitle = _.join(videoTitle, " ");
 
       queueSong(videoTitle, index)
-        .then((obj) => {
+        .then(obj => {
           servers[index].queue.ids.push(obj.id);
           servers[index].queue.names.push(obj.name);
           const url = `http://www.youtube.com/watch?v=${
@@ -67,39 +67,39 @@ client.on('message', (message) => {
           // if (!dispatcher) {
           message.reply(`Downloading: ${servers[index].queue.names[0]}`);
           downloadSong(message.guild.id, url)
-            .then((container) => {
+            .then(container => {
               const connection = message.guild.voiceConnection;
               const streamOptions = {
                 seek: 0,
                 volume: 0.2,
                 passes: 1,
-                bitrate: 48 * 1024,
+                bitrate: 48 * 1024
               };
               const streamDispatcher = connection.playFile(
                 `./public/${message.guild.id}.${container}`,
-                streamOptions,
+                streamOptions
               );
-              streamDispatcher.on('end', (reason) => {});
-              streamDispatcher.on('start', () => {
-                message.reply('Playing!');
+              streamDispatcher.on("end", reason => {});
+              streamDispatcher.on("start", () => {
+                message.reply("Playing!");
                 servers[index].queue.ids = _.drop(servers[index].queue.ids, 1);
                 servers[index].queue.names = _.drop(
                   servers[index].queue.names,
-                  1,
+                  1
                 );
               });
-              streamDispatcher.on('error', error => console.log(error));
+              streamDispatcher.on("error", error => console.log(error));
             })
             .catch(err => console.log(err));
           // }
         })
         .catch(err => console.log(err));
     } else {
-      message.reply('You must be in a voice channel.');
+      message.reply("You must be in a voice channel.");
     }
   }
 
-  if (_.includes(string, '!next')) {
+  if (_.includes(string, "!next")) {
     if (message.member.voiceChannel && servers[index].queue.ids.length > 0) {
       const url = `http://www.youtube.com/watch?v=${
         servers[index].queue.ids[0]
@@ -109,117 +109,117 @@ client.on('message', (message) => {
       message.guild.voiceConnection.dispatcher.end();
       message.reply(`Downloading: ${servers[index].queue.names[0]}`);
       downloadSong(message.guild.id, url)
-        .then((container) => {
+        .then(container => {
           const connection = message.guild.voiceConnection;
           const streamOptions = {
             seek: 0,
             volume: 0.2,
             passes: 1,
-            bitrate: 48 * 1024,
+            bitrate: 48 * 1024
           };
           const streamDispatcher = connection.playFile(
             `./public/${message.guild.id}.${container}`,
-            streamOptions,
+            streamOptions
           );
-          streamDispatcher.on('end', (reason) => {
+          streamDispatcher.on("end", reason => {
             // Message.reply('!next')
           });
-          streamDispatcher.on('start', () => {
-            message.reply('Playing!');
+          streamDispatcher.on("start", () => {
+            message.reply("Playing!");
             servers[index].queue.ids = _.drop(servers[index].queue.ids, 1);
             servers[index].queue.names = _.drop(servers[index].queue.names, 1);
           });
-          streamDispatcher.on('error', error => console.log(error));
+          streamDispatcher.on("error", error => console.log(error));
         })
         .catch(err => console.log(err));
       // }
     } else {
-      message.reply('You must be in a voice channel.');
+      message.reply("You must be in a voice channel.");
     }
   }
 
-  if (_.includes(string, '!resume')) {
+  if (_.includes(string, "!resume")) {
     if (message.member.voiceChannel) {
       var { dispatcher } = message.guild.voiceConnection;
       if (dispatcher) {
         if (dispatcher.paused) {
           dispatcher.resume();
-          message.reply('Playing!');
+          message.reply("Playing!");
         }
       }
     } else {
-      message.reply('You must be in a voice channel.');
+      message.reply("You must be in a voice channel.");
     }
   }
 
-  if (_.includes(string, '!pause')) {
+  if (_.includes(string, "!pause")) {
     if (message.member.voiceChannel) {
       var { dispatcher } = message.guild.voiceConnection;
       if (dispatcher) {
         if (!dispatcher.paused) {
           dispatcher.pause();
-          message.reply('Paused!');
+          message.reply("Paused!");
         }
       }
     } else {
-      message.reply('You must be in a voice channel.');
+      message.reply("You must be in a voice channel.");
     }
   }
 
-  if (_.includes(string, '!check')) {
+  if (_.includes(string, "!check")) {
     if (message.member.voiceChannel) {
       const str = `${servers[index].queue.names.length} songs queued \n${_.join(
         servers[index].queue.names,
-        '\n',
+        "\n"
       )}`;
       message.reply(str);
     }
   }
 
-  if (_.includes(string, '!remove')) {
+  if (_.includes(string, "!remove")) {
     if (message.member.voiceChannel) {
       const num = servers[index].queue.ids.length;
       if (num > 0) {
         message.reply(
-          `Song removed:  + ${servers[index].queue.names[num - 1]}`,
+          `Song removed:  + ${servers[index].queue.names[num - 1]}`
         );
         servers[index].queue.ids = _.drop(servers[index].queue.ids, 1);
         servers[index].queue.names = _.drop(servers[index].queue.names, 1);
       }
     } else {
-      message.reply('You must be in a voice channel.');
+      message.reply("You must be in a voice channel.");
     }
   }
 
-  if (_.includes(string, 'magic') && _.includes(string, 'conch')) {
+  if (_.includes(string, "magic") && _.includes(string, "conch")) {
     const answers = [
-      'Maybe.',
-      'Certainly not.',
-      'Not in your wildest dreams.',
-      'Nah, fam.',
-      'There is a good chance.',
-      'Quite likely.',
-      'I hope not.',
-      'Without a doubt.',
-      'I hope so.',
-      'Never!',
-      'Fuhgeddaboudit.',
-      'Pfft.',
-      'Very doubtful.',
-      'Sorry, bucko.',
-      'Hell, yes.',
-      'Hell to the no.',
-      'The future is bleak.',
-      'The future is uncertain.',
-      'I would rather not say.',
-      'Who cares?',
-      'Possibly.',
-      'Never, ever, ever.',
-      'There is a small chance.',
-      'Yes!',
-      'Obviously.',
-      'No.',
-      '42',
+      "Maybe.",
+      "Certainly not.",
+      "Not in your wildest dreams.",
+      "Nah, fam.",
+      "There is a good chance.",
+      "Quite likely.",
+      "I hope not.",
+      "Without a doubt.",
+      "I hope so.",
+      "Never!",
+      "Fuhgeddaboudit.",
+      "Pfft.",
+      "Very doubtful.",
+      "Sorry, bucko.",
+      "Hell, yes.",
+      "Hell to the no.",
+      "The future is bleak.",
+      "The future is uncertain.",
+      "I would rather not say.",
+      "Who cares?",
+      "Possibly.",
+      "Never, ever, ever.",
+      "There is a small chance.",
+      "Yes!",
+      "Obviously.",
+      "No.",
+      "42"
     ];
     const length = answers.length - 1;
     const distribution = random.integer(0, length);
@@ -227,73 +227,86 @@ client.on('message', (message) => {
     message.reply(answer);
   }
 
-  if (_.includes(string, 'd20')) {
+  if (_.includes(string, "d20")) {
     const number = Math.random() * (20 - 1) + 1;
     const d20 = _.toString(number);
     message.reply(d20);
   }
 
-  /* Not needed anymore
-	if (_.includes(string, 'tz ')) {
-		var msg = _.toUpper(string);
-		msg = _.split(msg, ' ');
+  if (_.includes(string, "tz ")) {
+    var msg = _.toUpper(string);
+    msg = _.split(msg, " ");
 
-		var index = msg.indexOf('TZ');
+    var index = msg.indexOf("TZ");
 
-		var time = msg[index + 1].split(':');
-		var hours = parseInt(time[0]);
-		var minutes = parseInt(time[1]);
+    var time = msg[index + 1].split(":");
+    var hours = parseInt(time[0]);
+    var minutes = parseInt(time[1]);
 
-		var tz = msg[index + 2];
-		var zone = getZone(tz);
+    var tz = msg[index + 2];
+    var zone = getZone(tz);
 
-		if (zone == null) {
-			message.reply('Unknown Timezone');
-			return;
-		}
+    if (zone == null) {
+      message.reply("Unknown Timezone");
+      return;
+    }
 
-		// Create date object with user inputted time in timezone
-		var now = moment_tz.tz(moment().utc(), zone);
-		var date = moment(now);
-		date.set('hours', hours + 12);
-		date.set('minutes', minutes);
+    // Create date object with user inputted time in timezone
+    var now = moment_tz.tz(moment().utc(), zone);
+    var date = moment(now);
+    date.set("hours", hours + 12);
+    date.set("minutes", minutes);
 
-		// Tz mutates date, does not return new one
-		message.reply(
-			'\n' + date.tz('America/New_York').zoneAbbr() + ':\t' + date.format('h:mm a') +
-			'\n' + date.tz('America/Chicago').zoneAbbr() + ':\t' + date.format('h:mm a') +
-			'\n' + date.tz('America/Denver').zoneAbbr() + ':\t' + date.format('h:mm a') +
-			'\n' + date.tz('America/Los_Angeles').zoneAbbr() + ':\t' + date.format('h:mm a') +
-			'\n' + date.tz('Europe/Dublin').zoneAbbr() + ':\t' + date.format('h:mm a')
-		);
-	}
-	*/
+    // Tz mutates date, does not return new one
+    message.reply(
+      "\n" +
+        date.tz("America/New_York").zoneAbbr() +
+        ":\t" +
+        date.format("h:mm a") +
+        "\n" +
+        date.tz("America/Chicago").zoneAbbr() +
+        ":\t" +
+        date.format("h:mm a") +
+        "\n" +
+        date.tz("America/Denver").zoneAbbr() +
+        ":\t" +
+        date.format("h:mm a") +
+        "\n" +
+        date.tz("America/Los_Angeles").zoneAbbr() +
+        ":\t" +
+        date.format("h:mm a") +
+        "\n" +
+        date.tz("Europe/Dublin").zoneAbbr() +
+        ":\t" +
+        date.format("h:mm a")
+    );
+  }
 });
 
 client.login(DISCORD_KEY);
 
-const getZone = (zone) => {
+const getZone = zone => {
   switch (zone) {
-    case 'EDT':
-    case 'EST':
-    case 'EASTERN':
-      return 'America/New_York';
-    case 'CDT':
-    case 'CST':
-    case 'CENTRAL':
-      return 'America/Chicago';
-    case 'MDT':
-    case 'MST':
-    case 'MOUNTAIN':
-      return 'America/Denver';
-    case 'PST':
-    case 'PDT':
-    case 'PACIFIC':
-      return 'America/Los_Angeles';
-    case 'GMT':
-    case 'IST':
-    case 'IRELAND':
-      return 'Europe/Dublin';
+    case "EDT":
+    case "EST":
+    case "EASTERN":
+      return "America/New_York";
+    case "CDT":
+    case "CST":
+    case "CENTRAL":
+      return "America/Chicago";
+    case "MDT":
+    case "MST":
+    case "MOUNTAIN":
+      return "America/Denver";
+    case "PST":
+    case "PDT":
+    case "PACIFIC":
+      return "America/Los_Angeles";
+    case "GMT":
+    case "IST":
+    case "IRELAND":
+      return "Europe/Dublin";
     default:
       return null;
   }
