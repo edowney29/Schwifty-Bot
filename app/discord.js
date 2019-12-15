74eb7d23c15b850c1413339cc621b61599daf4d7
Eric Downey <edowney29@gmail.com>
2019-12-15 16:48:19 -0500

    wow deathroll

--- app/discord.js
+++ app/discord.js
@@ -2,9 +2,16 @@ const discord = require("discord.js");
 
 // const helper = require("./helper");
 
-const { DISCORD_KEY } = process.env;
+// const { DISCORD_KEY } = process.env;
+const DISCORD_KEY =
+  "MzMwNTM5ODQ0ODg5NDc3MTIx.XakZpw.zC1Sngu8sCAXa-4E-STIhU34vq8";
 
-const servers = [];
+// const servers = [];
+const deathrolls = {
+  offers: {},
+  battles: [],
+  payments: []
+};
 
 module.exports = () => {
   try {
@@ -16,18 +23,19 @@ module.exports = () => {
     });
 
     client.on("message", message => {
-      let index = servers.findIndex(elem => elem.id === message.guild.id);
-      if (index === -1) {
-        const server = {
-          id: message.guild.id,
-          queue: {
-            ids: [],
-            names: []
-          }
-        };
-        servers.push(server);
-        index = servers.length - 1;
-      }
+      // let index = servers.findIndex(elem => elem.id === message.guild.id);
+      // if (index === -1) {
+      //   const server = {
+      //     id: message.guild.id,
+      //     queue: {
+      //       ids: [],
+      //       names: []
+      //     }
+      //   };
+      //   servers.push(server);
+      //   index = servers.length - 1;
+      // }
+      if (message.author.id === "330539844889477121") return;
 
       const string = message.content.toLowerCase();
       console.log(
@@ -96,7 +104,7 @@ module.exports = () => {
         let rolls = getNumber(strArray[1], false);
         let dice = getNumber(strArray[2], true);
         let plus = getNumber(strArray[3], false);
-        let other = getNumber(strArray[4], true);
+        // let other = getNumber(strArray[4], true);
         if (!rolls) rolls = 1;
 
         const numArray = [];
@@ -152,23 +160,210 @@ module.exports = () => {
       }
 
       if (string.includes("/roll")) {
+        message.delete();
         const strArr = string.split(" ");
         if (strArr.length === 1) {
-          message.reply(Math.floor(Math.random() * 100) + 1);
+          const roll = Math.floor(Math.random() * 100) + 1;
+          message.channel.send(
+            `${idToMention(message.author.id)} rolls ${roll} (1-100)`
+          );
         } else if (strArr.length === 2) {
           const max = getNumber(strArr[1]);
-          message.reply(Math.floor(Math.random() * max) + 1);
+          const roll = Math.floor(Math.random() * max) + 1;
+          message.channel.send(
+            `${idToMention(message.author.id)} rolls ${roll} (1-${max})`
+          );
+          doBattleStep(message.author.id, roll, max, message);
         } else if (strArr.length === 3) {
           const min = getNumber(strArr[1]);
           const max = getNumber(strArr[2]);
-          message.reply(Math.floor(Math.random() * max) + min);
+          const roll = Math.floor(Math.random() * max) + min;
+          message.channel.send(
+            `${idToMention(message.author.id)} rolls ${roll} (${min}-${max})`
+          );
         } else {
-          message.reply("oof");
+          message.channel.send("oof try again");
         }
+      }
+
+      if (string.includes("/offer")) {
+        message.delete();
+        const index = getBattleIndex(message.author.id);
+        if (~index) {
+          message.channel.send(
+            `${idToMention(message.author.id)} you're in a deathroll with ${
+              message.author.id === deathrolls.battles[index].offer
+                ? idToMention(deathrolls.battles[index].accept)
+                : idToMention(deathrolls.battles[index].offer)
+            }`
+          );
+        } else {
+          const strArr = string.split(" ");
+          if (strArr.length === 1) {
+            message.channel.send(
+              `${idToMention(message.author.id)} add a gold amount /offer 100`
+            );
+          } else if (strArr.length === 2) {
+            const gold = getNumber(strArr[1]) || 100;
+            if (deathrolls.offers[message.author.id])
+              message.channel.send(
+                `${idToMention(
+                  message.author.id
+                )} new offer: ${gold} gold deathroll`
+              );
+            else
+              message.channel.send(
+                `${idToMention(
+                  message.author.id
+                )} offers a ${gold} gold deathroll`
+              );
+            deathrolls.offers[message.author.id] = gold;
+          } else {
+            message.channel.send("oof try again");
+          }
+        }
+      }
+
+      if (string.includes("/accept")) {
+        message.delete();
+        const index = getBattleIndex(message.author.id);
+        if (~index) {
+          message.channel.send(
+            `${idToMention(message.author.id)} you're in a deathroll with ${
+              message.author.id === deathrolls.battles[index].offer
+                ? idToMention(deathrolls.battles[index].accept)
+                : idToMention(deathrolls.battles[index].offer)
+            }`
+          );
+        } else {
+          const strArr = string.split(" ");
+          if (strArr.length === 1) {
+            message.channel.send(
+              `${idToMention(
+                message.author.id
+              )} add a username /accept @SchwifyBot`
+            );
+          } else if (strArr.length === 2) {
+            const user = getUserFromMention(strArr[1]);
+            if (!user) {
+              message.channel.send(
+                `${idToMention(message.author.id)} no user was found`
+              );
+            } else if (deathrolls.offers[user.id]) {
+              deathrolls.battles.push({
+                accept: message.author.id,
+                offer: user.id,
+                gold: deathrolls.offers[user.id],
+                firstroll: true,
+                lastroll: null
+              });
+              message.channel.send(
+                `${idToMention(user.id)} ${
+                  deathrolls.offers[user.id]
+                } gold offer was accepted. ${idToMention(
+                  message.author.id
+                )} /roll ${deathrolls.offers[user.id] * 10}`
+              );
+              delete deathrolls.offers[message.author.id];
+              delete deathrolls.offers[user.id];
+            } else {
+              message.channel.send(
+                `${idToMention(user.id)} doesn't have any offers`
+              );
+            }
+          } else {
+            message.channel.send("oof try again");
+          }
+        }
+      }
+
+      if (string.includes("/surrender")) {
+        const index = getBattleIndex(message.author.id);
+        if (~index) {
+          deathrolls.battles.splice(index, 1);
+        }
+      }
+
+      if (string.includes("/give")) {
+        message.delete();
+        message.channel.send("oof try again");
+      }
+
+      if (string.includes("/leger")) {
+        message.delete();
+        message.channel.send("oof try again");
       }
     });
 
+    client.on("error", error => console.error(error));
+
     client.login(DISCORD_KEY);
+
+    const getUserFromMention = mention => {
+      // The id is the first and only match found by the RegEx.
+      const matches = mention.match(/^<@!?(\d+)>$/);
+
+      // If supplied variable was not a mention, matches will be null instead of an array.
+      if (!matches) return;
+
+      // However the first element in the matches array will be the entire mention, not just the ID,
+      // so use index 1.
+      const id = matches[1];
+
+      return client.users.get(id);
+    };
+
+    const getBattleIndex = id => {
+      return deathrolls.battles.findIndex(
+        battle => battle.offer === id || battle.accept === id
+      );
+    };
+
+    const doBattleStep = (id, roll, max, message = null) => {
+      const index = getBattleIndex(id);
+      if (~index) {
+        if (
+          deathrolls.battles[index].firstroll
+            ? id === deathrolls.battles[index].accept
+            : id === deathrolls.battles[index].offer
+        ) {
+          if (!deathrolls.battles[index].lastroll)
+            deathrolls.battles[index].lastroll = roll;
+          else if (deathrolls.battles[index].lastroll === max) {
+            if (roll === 1) {
+              message.channel.send(
+                `${
+                  deathrolls.battles[index].firstroll
+                    ? idToMention(deathrolls.battles[index].accept)
+                    : idToMention(deathrolls.battles[index].offer)
+                } is a loser and owes ${
+                  !deathrolls.battles[index].firstroll
+                    ? idToMention(deathrolls.battles[index].accept)
+                    : idToMention(deathrolls.battles[index].offer)
+                } ${deathrolls.battles[index].gold} gold`
+              );
+              deathrolls.battles.splice(index, 1);
+            } else {
+              deathrolls.battles[index].firstroll = !deathrolls.battles[index]
+                .firstroll;
+              deathrolls.battles[index].lastroll = roll;
+            }
+          } else {
+            message.channel.send(
+              `${idToMention(message.author.id)} finish your deathroll with ${
+                message.author.id === deathrolls.battles[index].offer
+                  ? idToMention(deathrolls.battles[index].accept)
+                  : idToMention(deathrolls.battles[index].offer)
+              } /roll ${deathrolls.battles[index].lastroll}`
+            );
+          }
+        }
+      }
+    };
+
+    const idToMention = id => {
+      return `<@${id}>`;
+    };
   } catch (err) {
     console.log(err);
   }
